c06d27e46154a0b86acc321783b5de8a81dd5314
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-01-28 00:09:42 +0300

    Add shortcut for scroll functions in st

--- st/config.h
+++ st/config.h
@@ -199,6 +199,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,          XK_q,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_a,           kscrolldown,    {.i = -1} },
 };
 
 /*
