e5d2be73390f49cf7895dad5479da8b1b48eec4c
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-03-05 00:37:53 +0300

    Refactoring focus_window script

--- config/i3/focus_window.py
+++ config/i3/focus_window.py
@@ -1,27 +1,59 @@
 #!/usr/bin/env python3
 
-from i3ipc import Connection, Event
+from i3ipc import Con, Connection, Event
+import os
 import subprocess
 
-i3 = Connection()
+class Window(Con):
 
-focused = i3.get_tree()
+    def __init__(self, data, parent, conn):
+        Con.__init__(self, data, parent, conn)
+        self._save_last_workspace()
 
-windows = []
 
-for con in i3.get_tree():
-    if con.window and con.parent.type != 'dockarea' and con.workspace().name != '__i3_scratch':
-        con.last_workspace = con.workspace().name
-        con.command("move container to workspace tmp");
-        windows.append(con);
+    def move_to_workspace(self, workspace):
+        self._save_last_workspace()
+        super().command("move container to workspace %s" % workspace);
+        
+    def _save_last_workspace(self):
+        self.last_workspace = super().workspace().name
+        
+class Manager:
+    
+    i3 = Connection()
 
-i3.command("workspace tmp")
+    def move_all_windows(workspace):
+        windows = []
+        for con in Manager.i3.get_tree():
+            if con.window and con.parent.type != 'dockarea' and con.workspace().name != '__i3_scratch':
+                window = Window(con.ipc_data, con.parent, con._conn)
+                window.move_to_workspace(workspace)
+                windows.append(window)
+        return windows
+    
+    def focus_workspace(workspace):
+        Manager.i3.command("workspace %s" % workspace)
 
-subprocess.call(["/home/foobar/.config/i3/winswitch/winswitch"])
+    def focus_window(container):
+        Manager.i3.command('[id="%d"] focus' % container.window)
 
-focused = i3.get_tree().find_focused()
+    def choose_window():
+        subprocess.call([os.path.expanduser('~/.config/i3/winswitch/winswitch')])
+        return Manager.i3.get_tree().find_focused()
 
-for con in windows:
-    con.command("move container to workspace %s" % con.last_workspace);
+    def restore_workspaces(windows):
+        for container in windows:
+            container.move_to_workspace(container.last_workspace);
 
-i3.command('[id="%d"] focus' % focused.window)
+
+
+windows = Manager.move_all_windows("tmp")
+
+Manager.focus_workspace("tmp")
+
+focused_window = Manager.choose_window()
+
+Manager.restore_workspaces(windows);
+
+Manager.focus_window(focused_window)
+
