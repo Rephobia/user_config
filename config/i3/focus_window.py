443f27dfab07009033180511a48bf14043f54941
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-03-14 00:39:27 +0300

    Rename manage.py

--- config/i3/focus_window.py
+++ config/i3/focus_window.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-import manager
+import i3
 import winswitch
 
 if __name__ == "__main__":
     
-    memorycons = manager.move_windows("tmp")
+    memorycons = i3.move_windows("tmp")
 
-    manager.focus_workspace("tmp")
+    i3.focus_workspace("tmp")
 
     winid = winswitch.choose_window()
     
-    manager.restore_workspaces(memorycons);
+    i3.restore_workspaces(memorycons);
     
-    manager.focus_window(winid)
+    i3.focus_window(winid)
 
