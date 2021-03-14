42742c061a169f3a27d975ce477fcae36dfba8ab
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-03-14 13:20:43 +0300

    add layout in tmp workspace

--- config/i3/focus_window.py
+++ config/i3/focus_window.py
@@ -9,9 +9,11 @@ if __name__ == "__main__":
 
     i3.focus_workspace("tmp")
 
+    i3.set_layout(memorycons, rowsize=3)
+    
     winid = winswitch.choose_window()
     
-    i3.restore_workspaces(memorycons);
+    i3.restore_workspaces(memorycons)
     
     i3.focus_window(winid)
 
--- config/i3/i3.py
+++ config/i3/i3.py
@@ -25,3 +25,23 @@ def focus_window(winid : int):
 def restore_workspaces(memorycons: List[MemoryCon]):
     for window in memorycons:
         window.move_to_workspace(window.last_workspace);
+
+def set_layout(memorycons: List[MemoryCon], rowsize):
+
+    for con in memorycons[:rowsize]:
+        con.command("split v")
+
+    for row in _chunk(memorycons[rowsize:], rowsize):
+        step = rowsize
+        for con in row:
+            con.command("split v")
+            _move_left(con, step)
+            step -= 1
+
+def _move_left(con: MemoryCon, step: int):
+    for i in range(step):
+        con.command("move left")
+        
+def _chunk(memorycons: List[MemoryCon], n):
+    for i in range(0, len(memorycons), n):
+        yield memorycons[i:i + n]
