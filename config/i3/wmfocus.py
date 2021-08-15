03c8a1a721d282c0074c5f892b77b0dde374635e
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-08-15 22:59:31 +0300

    Change wmfocus options

--- config/i3/wmfocus.py
+++ config/i3/wmfocus.py
@@ -6,9 +6,9 @@ import subprocess
 def choose_window(halign: str = "center",
                   valign: str = "center",
                   font: str = "'Droid Sans':100",
-                  bgcolor: str = "#204a87",
-                  textcolor: str = "#eeeeec",
-                  chars: str = "qweasd") -> int:
+                  bgcolor: str = "rgba(50, 50, 50, 0.7)",
+                  textcolor: str = "rgba(255, 255, 255, 1)",
+                  chars: str = "wertdf") -> int:
     
     path = os.path.expanduser("~/.cargo/bin/wmfocus")
 
@@ -20,6 +20,7 @@ def choose_window(halign: str = "center",
                                      "--bgcolor", str(bgcolor),
                                      "--textcolor", str(textcolor),
                                      "--chars", str(chars),
+                                     "--fill",
                                      "--exit-keys", "Control_L+g"
                                      ])
     return int(winid, 16)
