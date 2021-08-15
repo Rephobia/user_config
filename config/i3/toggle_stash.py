f8226dee6edaa0f16b400e4cbc7dda9890741eb0
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-08-15 23:23:44 +0300

    Remove wmfocus if count of windows is less than one

--- config/i3/toggle_stash.py
+++ config/i3/toggle_stash.py
@@ -15,10 +15,13 @@ if __name__ == "__main__":
 
     stash_prefix = "s:"
     workspace_name = i3.get_tree().find_focused().workspace().name
-    
     if workspace_name.startswith(stash_prefix):
         i3.command("workspace %s" % remove_prefix(stash_prefix, workspace_name))
     else:
         i3.command("workspace %s" % stash_prefix + remove_prefix(stash_prefix, workspace_name))
-        i3.command('[id="%s"] focus' % wmfocus.choose_window())
-
+        windows = 0;
+        for con in i3.get_tree():
+            if (con.window and con.parent.type != 'dockarea' and con.workspace().name == stash_prefix + remove_prefix(stash_prefix, workspace_name)):
+                windows += 1
+        if windows > 1:
+            i3.command('[id="%s"] focus' % wmfocus.choose_window())
