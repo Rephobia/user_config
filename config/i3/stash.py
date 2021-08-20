c6ceb516000ae95579e0fa60d96a5cd4023131e4
Rephobia <26533256+Rephobia@users.noreply.github.com>
2021-08-20 23:17:30 +0300

    Change prefix to suffix to stash workspaces

--- config/i3/stash.py
+++ config/i3/stash.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 
-# We have two workspace types, major and minor. Minor workspace names starts with _PREFIX
-_PREFIX = "s:"
+# We have two workspace types, major and minor. Minor workspace names starts with _SUFFIX
+_SUFFIX = ":s"
 
-def _remove_prefix(text: str):
-    if text.startswith(_PREFIX):
-        return text[len(_PREFIX):]
+def _remove_sufix(text: str):
+    if text.endswith(_SUFFIX):
+        print(text[:len(_SUFFIX)])
+        return text[:-len(_SUFFIX)]
     return text
 
 # Checks if workspace name is minor
 def is_minor(workspace: str) -> bool:
-    return workspace.startswith(_PREFIX)
+    return workspace.endswith(_SUFFIX)
 
 # Construct major workspace name
 def major(workspace: str) -> str:
-    return _remove_prefix(workspace)
+    return _remove_sufix(workspace)
 
 # Construct minor workspace name
 def minor(workspace: str) -> str:
-    return _PREFIX + _remove_prefix(workspace)
+    return _remove_sufix(workspace) + _SUFFIX
   
     
