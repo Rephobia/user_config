6cd020c4ab321f84243c6b1151f33238a50e2bfc
Rephobia <26533256+Rephobia@users.noreply.github.com>
2022-05-17 10:57:01 +0300

    Refactoring main builder

--- builder.py
+++ builder.py
@@ -2,31 +2,40 @@
 
 import argparse
 import sys
-
 import builders
+
+from types import ModuleType
+from typing import Callable
 
 class ConsoleColors:
     ERROR = '\033[0;31m'
     ENDC = '\033[0m'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('mode', type=str, help='What commands to use, (build, unbuild)')
-args = parser.parse_args()
+def get_current_mode(user_passed_mode: str) -> Callable:
+    modes = {
+        'build': lambda x: x.build(),
+        'unbuild': lambda x: x.unbuild()
+    }
 
-modes = {
-    'build': lambda x: x.build(),
-    'unbuild': lambda x: x.unbuild()
-}
+    mode = modes.get(user_passed_mode, None)
 
-mode = modes.get(args.mode, None)
+    if not mode:
+        print(ConsoleColors.ERROR + '\nWrong command line argument\n' + ConsoleColors.ENDC)
+        parser.print_help()
+        sys.exit()
 
-if not mode:
-    print(ConsoleColors.ERROR + '\nWrong command line argument\n' + ConsoleColors.ENDC)
-    parser.print_help()
-    sys.exit()
+    return mode
 
-for builder in builders.get_builders():
-    try:
-        modes[args.mode](builder)
-    except Exception as error:
-        print(ConsoleColors.ERROR + 'Error: ' + ConsoleColors.ENDC, error)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', type=str, help='What commands to use, (build, unbuild)')
+    args = parser.parse_args()
+
+    mode = get_current_mode(args.mode)
+
+    for builder in builders.get_builders():
+        try:
+            mode(builder)
+        except Exception as error:
+            print(ConsoleColors.ERROR + 'Error: ' + ConsoleColors.ENDC, error)
