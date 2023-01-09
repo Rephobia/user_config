59f8dfbcd4cc2d439ca344f74d8563d882d97d53
Rephobia <26533256+Rephobia@users.noreply.github.com>
2023-01-09 23:13:35 +0300

    Focus prefix workspace if current stash has only one workspace

--- playbooks/files/i3/i3/scripts/dmenu_stash_focus.py
+++ playbooks/files/i3/i3/scripts/dmenu_stash_focus.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Get stash workspaces exclude current and pass they in dmenu.
-Chosen workspace will be focused
+Get stash workspaces exclude current and pass they in dmenu. 
+Chosen workspace will be focused.
+If stash have only two workspaces toggle switch between them, without dmenu
+If stash empty, focus prefix workspace.
 """
 
 from i3ipc import Connection
@@ -20,4 +22,5 @@ if __name__ == "__main__":
         else:
             Connection().command("workspace %s" % workspaces[0].name)
     else:
-        Dmenu.show_values(["Empty stash"])
+        ws_current = Connection().get_tree().find_focused().workspace().name
+        Connection().command("workspace %s" % Stash.get_workspace_prefix(ws_current))
